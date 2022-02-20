6773ecc94c833dda5721a407af7e374ab077548e
Allan Gikonyo <gikonyo257@gmail.com>
2022-02-20 22:39:42 +0300

    added testuser class

---
+++ user-test.py
@@ -0,0 +1,12 @@
+import unittest # Importing the unittest module
+from user import user # Importing the user class
+
+
+class Testuser(unittest.Testcase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
