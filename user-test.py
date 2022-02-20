a6a7d94c6d2bc7be5b0d080e2c523fc92d8b2018
Allan Gikonyo <gikonyo257@gmail.com>
2022-02-20 22:48:49 +0300

    added setup method and test methods

--- user-test.py
+++ user-test.py
@@ -9,4 +9,26 @@ class Testuser(unittest.Testcase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+def setUp(self):
+    '''
+    Set up method to run before each test cases.
+    '''
+    self.new_user = user("Allan", "Gikonyo", "2022")
+
+def test_init(self):
+    
+    '''
+    test_init test case to test if the object is initialized properly
+    '''
+    self.assertEqual(self.new_user.user_name, "Allan")
+
+def test_save_user(self):
+
+    '''
+    test_save_contact test case to test if the contact object is saved into the contact list
+    '''
+    self.new_user.save_user()
+    self.assertEqual(len(user.user_list), 1)
+
+
 
